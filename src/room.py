72a3b554fb349f3d8e12ca37b90cd85e1fd2e967
Alex <agordon1123@gmail.com>
2019-12-04 19:48:46 -0500

    created Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        print(f"You are currently {self.name}")
+        print(f"{self.description}")
+        print(f"n: {self.n_to}, s: {self.s_to}, e: {self.e_to}, w: {self.w_to}")
